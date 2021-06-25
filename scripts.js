4f2c61eefdfafb87bd22c2d01f56bdfc7ab5a37a
prasantamang <prashantamang059@gmail.com>
2021-06-25 12:24:44 +0545

    newfile

---
+++ scripts.js
@@ -0,0 +1,58 @@
+//q.no.4
+var number = 120;
+console.log(number);
+
+var firstString = "5150";
+console.log(firstString);
+
+var secondString = "Two Hundred Thirty";
+console.log (secondString);
+
+//q.no.5
+var threeNumber =["1", "2","3"];
+console.log(threeNumber);
+var twoString=["yo hoo","hello"];
+console.log(twoString);
+
+//q.no.6
+alert("Steave's response was \"cool\"");
+
+//q.no.7 
+var integer = parseInt(firstString,10);
+console.log(integer);
+
+var secondInteger = parseInt(secondInteger,10);
+console.log(secondInteger,10);
+
+//qno 8
+var a = parseInt(prompt("Enter first number"));
+var b = parseInt(prompt("Enter second number"));
+alert(a + b);
+
+var num= 10;
+var strn= "i was ";
+alert( strn + num);
+
+var x="yoo";
+var y="lets's go";
+alert(x+y);
+
+//q no 9
+var before= 3;
+alert(before);
+
+after= before++;
+alert(before);
+
+//q no 10
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(num));
+console.log(typeof(strn));
+console.log(typeof(x));
+console.log(typeof(y));
+
+//q no 11
+
+
+
